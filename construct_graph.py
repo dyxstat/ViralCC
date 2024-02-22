041ee7cae87ddb3283268e2bef0f5c982980bf54
Yuxuan Yancey Du <55218554+dyxstat@users.noreply.github.com>
2024-02-21 21:16:47 -0800

    Update construct_graph.py

--- construct_graph.py
+++ construct_graph.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Copyright (c) 2019 Matthew Z. DeMaere
+# Modifications Copyright (c) 2022 Yuxuan Du
+# 2022-03-05: The class Sparse2DAccumulator and ContactMatrix were modified from https://github.com/cerebis/bin3C.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from collections import OrderedDict, namedtuple
 import Bio.SeqIO as SeqIO
 from Bio.SeqUtils import GC
